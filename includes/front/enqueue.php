b7164f49164cab1ebce3c1c2e02da2fb9baed602
AlexKor94 <sasha1994work@gmail.com>
2023-04-26 08:09:16 +0300

    enqueue scripts

--- includes/front/enqueue.php
+++ includes/front/enqueue.php
@@ -1,4 +1,5 @@
 <?php
+// https://developer.wordpress.org/reference/functions/wp_enqueue_script/
 function ap_enqueue()
 {
   wp_register_style(
@@ -7,4 +8,10 @@ function ap_enqueue()
   );
 
   wp_enqueue_style('ap_theme');
+
+  wp_enqueue_script('jquery-script', get_template_directory_uri() . '/js/jquery.min.js', array('jquery'), '1.0', true);
+  wp_enqueue_script('browser-script', get_template_directory_uri() . '/js/browser.min.js', array('jquery'), '1.0', true);
+  wp_enqueue_script('breakpoints-script', get_template_directory_uri() . '/js/breakpoints.min.js', array('jquery'), '1.0', true);
+  wp_enqueue_script('util-script', get_template_directory_uri() . '/js/util.js', array('jquery'), '1.0', true);
+  wp_enqueue_script('main-script', get_template_directory_uri() . '/js/main.js', array('jquery'), '1.0', true);
 }
